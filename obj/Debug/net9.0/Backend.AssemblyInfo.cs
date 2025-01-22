601762a7bb428986b026a2a6077c6e4db0c16a50
codemedic213515 <13720882+codemedic213515@users.noreply.github.com>
2025-01-23 04:03:28 +0900

    update create main

--- obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09161b2aa49a82d906a0a294d45cb5a5285affe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ddc6a8b3f6a487ce1086f639ea8a42d8174ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
